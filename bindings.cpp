0ca0f25e8a3450eb5eea78cd8059a04d35af1701
cobirnm <45829329+cobirnm@users.noreply.github.com>
2021-12-02 16:35:36 +0000

    Update bindings.cpp

--- bindings.cpp
+++ bindings.cpp
@@ -1897,6 +1897,7 @@ bool DeRestPluginPrivate::sendConfigureReportingRequest(BindingTask &bt)
                  modelId.startsWith(QLatin1String("3157100")) ||
                  modelId == QLatin1String("URC4450BC0-X-R") || // Xfinity Keypad XHK1-UE / URC4450BC0-X-R
                  modelId == QLatin1String("3405-L") || // IRIS 3405-L Keypad
+		 modelId == QLatin1String("3400") || // Centralite 3400 Keypad
                  modelId.startsWith(QLatin1String("4655BC0")))
         {
             rq.attributeId = 0x0020;   // battery voltage
@@ -3196,6 +3197,8 @@ bool DeRestPluginPrivate::checkSensorBindingsForAttributeReporting(Sensor *senso
         sensor->modelId() == QLatin1String("URC4450BC0-X-R") ||
         // Iris
         sensor->modelId() == QLatin1String("3405-L") ||
+        // Centralite
+        sensor->modelId() == QLatin1String("3400") ||
         // Eria
         sensor->modelId() == QLatin1String("Adurolight_NCC")
         )
@@ -3374,6 +3377,7 @@ bool DeRestPluginPrivate::checkSensorBindingsForAttributeReporting(Sensor *senso
                      sensor->modelId().startsWith(QLatin1String("4655BC0")) ||
                      sensor->modelId() == QLatin1String("URC4450BC0-X-R") || // Xfinity Keypad XHK1-UE
                      sensor->modelId() == QLatin1String("3405-L") || // IRIS 3405-L Keypad
+		     sensor->modelId() == QLatin1String("3400") || // Centralite 30400 Keypad
                      sensor->modelId() == QLatin1String("113D"))
             {
                 val = sensor->getZclValue(*i, 0x0020); // battery voltage
@@ -3966,6 +3970,7 @@ bool DeRestPluginPrivate::checkSensorBindingsForClientClusters(Sensor *sensor)
              sensor->modelId().startsWith(QLatin1String("RC-EM")) ||
              sensor->modelId() == QLatin1String("URC4450BC0-X-R") ||
              sensor->modelId() == QLatin1String("3405-L") ||
+	     sensor->modelId() == QLatin1String("3400") ||
              sensor->modelId().startsWith(QLatin1String("RC-EF-3.0")))
     {
         clusters.push_back(IAS_ACE_CLUSTER_ID);
